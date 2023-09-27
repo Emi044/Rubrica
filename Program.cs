95b29206eea734a5a02f4f65958d1c5c8dba568f
emineri04 <emineri04@gmail.com>
2023-09-27 11:34:48 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+const string file = "rubrica.json";
+
+Lista();
+
+string str = Console.ReadLine();
+
+if (args.Length > 0)
+{
+    if (args[0] == "lista")
+        Lista();
+    else if (args[0] == "cerca")
+    {
+        ReadRubrica().ForEach(contact =>
+        {
+            if (contact.Contains(args[1]))
+                Console.WriteLine(contact.Nome);
+        });
+    }
+}
+
+
+Contatto Deserializza(string json)
+{
+    Contatto? contatto = JsonSerializer.Deserialize<Contatto>(json);
+    if (contatto == null)
+    {
+        throw new Exception("JSON non valido.");
+    }
+
+    return contatto;
+}
+void Lista()
+{
+    foreach (Contatto contatto in ReadRubrica())
+    {
+        Console.WriteLine(contatto.Nome + ' ' + contatto.Cognome + ": " + contatto.Numero);
+    }
+}
+
+List<Contatto> ReadRubrica()
+{
+    return File.ReadLines(file)
+        .Select(Deserializza).ToList();
+}
+class Contatto
+{
+    public string Nome { get; set; } = "";
+    public string Cognome { get; set; } = "";
+    public string Numero { get; set; } = "";
+
+    public Boolean Contains(string s)
+    {
+        return this.Nome.Contains(s) ||
+                this.Cognome.Contains(s) ||
+                this.Numero.Contains(s);
+    }
+}
