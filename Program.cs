b7903edabc5ab09bc9080a5432b9a48ba2cfbb17
emineri04 <emineri04@gmail.com>
2023-09-27 16:50:24 +0200

    wip

--- Program.cs
+++ Program.cs
@@ -3,28 +3,71 @@ using System.Text.Json.Serialization;
 
 const string file = "rubrica.json";
 
-Lista(ReadRubrica());
+List<Contatto> contattos = await ReadRubrica();
 
-var str = Console.ReadLine();
+List<string> azione;
+azione = args.ToList();
 
-if (args.Length > 0)
+while (!CheckArgs(azione))
 {
-    if (args[0] == "lista")
-        Lista(ReadRubrica());
-    else if (args[0] == "cerca")
+    Console.WriteLine("Non è stata specificata un'azione, cosa desideri fare? [lista, ricerca, nuovo] ");
+    Console.Write("Azione -> ");
+    azione = Console.ReadLine().Split(' ').ToList();
+}
+if (azione[0] == "lista")
+    Lista(contattos);
+else if (azione[0] == "ricerca")
+{
+    if (azione.Count == 2) Lista(Cerca(azione[1]));
+    else
     {
-        Lista(Cerca(args[1]));
+        Console.WriteLine("Non è stato specificato nessuno parametro di ricerca");
+        Lista(Cerca(Console.ReadLine()));
     }
 }
-else
+else if (azione[0] == "nuovo")
 {
-    Console.WriteLine("ciao");
+    if (azione.Count == 4) AddContatto(azione[1], azione[2], azione[3]);
+    else if (azione.Count == 3)
+    {
+        Console.Write("Inserire il numero di telefono ->");
+        string nu = Console.ReadLine();
+        AddContatto(azione[1], azione[2], nu);
+    }
+    else if (azione.Count == 2)
+    {
+        Console.Write("Inserire il cognome ->");
+        string co = Console.ReadLine();
+        Console.Write("Inserire il numero di telefono ->");
+        string nu = Console.ReadLine();
+        AddContatto(azione[1], co, nu);
+    }
+    else
+    {
+        Console.Write("Inserire il nome ->");
+        string no = Console.ReadLine();
+        Console.Write("Inserire il cognome ->");
+        string co = Console.ReadLine();
+        Console.Write("Inserire il numero di telefono ->");
+        string nu = Console.ReadLine();
+        AddContatto(no, co, nu);
+    }
+}
+
+void AddContatto(string nome, string cognome, string numero)
+{
+    Contatto c = new Contatto();
+    c.Nome = nome;
+    c.Cognome = cognome;
+    c.Numero = numero;
+    contattos.Add(c);
+    File.AppendAllText("rubrica.json", $"\n{JsonSerializer.Serialize(c)}");
 }
 
 List<Contatto> Cerca(string s)
 {
     var l = new List<Contatto>();
-    foreach (var c in ReadRubrica())
+    foreach (Contatto c in contattos)
     {
         if (c.Contains(s)) l.Add(c);
     }
@@ -40,6 +83,7 @@ Contatto Deserializza(string json)
     }
 
     return contatto;
+
 }
 void Lista(List<Contatto> l)
 {
@@ -49,10 +93,18 @@ void Lista(List<Contatto> l)
     }
 }
 
-List<Contatto> ReadRubrica()
+async Task<List<Contatto>> ReadRubrica() 
 {
-    return File.ReadLines(file)
-        .Select(Deserializza).ToList();
+    return (await File.ReadAllLinesAsync(file)).Select(Deserializza).ToList();
+}
+
+Boolean CheckArgs(List<String> azioni)
+{
+    if (azioni.Count == 0) return false;
+    if (azioni[0] == "lista" && azioni.Count == 1) return true;
+    else if (azioni[0] == "ricerca" && (azioni.Count == 2 || azioni.Count == 1)) return true;
+    else if (azioni[0] == "nuovo" && (azioni.Count >= 1 || azioni.Count <= 4)) return true;
+    else return false;
 }
 class Contatto
 {
@@ -62,8 +114,8 @@ class Contatto
 
     public Boolean Contains(string s)
     {
-        return this.Nome.Contains(s) ||
-                this.Cognome.Contains(s) ||
-                this.Numero.Contains(s);
+        return this.Nome.ToLower().Contains(s.ToLower()) ||
+                this.Cognome.ToLower().Contains(s.ToLower()) ||
+                this.Numero.ToLower().Contains(s.ToLower());
     }
 }
